1081a15f664a2144db593b246a060bbaf5d3759f
Marcos Vinicius <marcos.oliveira@mjv.com.br>
2018-06-22 09:27:33 -0300

    Updating files in production, JS functions for Angular JS, SASS and layout

--- dev/assets/js/core.js
+++ dev/assets/js/core.js
@@ -1,7 +1,11 @@
 angular.module('gestao-app', []).controller("gestao-app-controller", function($scope) {
-  $scope.arrSidebar = [
-      { isActive: true, text: 'Painel de Gestão', icon: '' },
-      { text: 'Ant. de Recebíveis', icon: '' },
-      { text: 'Conta Digital', icon: '' }
-  ]
+  $scope.arrSidebar = [{ isFilter: true, text: 'Pesquise Aqui', icon: 'lupa' },
+      { isActive: true, text: 'Painel de Gestão', icon: 'globo' },
+      { text: 'Ant. de Recebíveis', icon: 'arroba' },
+      { text: 'Conta Digital', icon: 'refresh', after: 'seta-dupla' }]
+  $scope.arrShortcuts = {operacional: ['Envio', 'Envio de Duplicatas', 'Nova Operação', 'Assinatura', 'Assinatura Digital'],
+    geral: ['Carteira', 'Instruções']}
+    $scope.arrAlerts = [{ text: '23 Notas Canceladas' },
+    { text: '13 Notas Canceladas', isAction: true },
+    { text: '7 Assinaturas Digital', isAction: true }]
 });
